74eda9a49cda2e7c09fb3c3c41a8911e63abb958
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-11-01 14:35:05 -0400

    Handling nil user for WeightedQueryStrategy
    Prior to this commit, passing nil as the user would've raised an
    exception (or perhaps more confounding query that involved joining on
    null values).
    
    With this commit we leverage the similar logic where a user is not required.

--- app/services/articles/feeds/weighted_query_strategy.rb
+++ app/services/articles/feeds/weighted_query_strategy.rb
@@ -32,6 +32,10 @@ module Articles
       #          factor.
       #
       # - fallback: When no case is matched use this factor.
+      #
+      # - requires_user: Does this scoring method require a given
+      #                  user.  If not, don't use it if we don't have
+      #                  a nil user.
       SCORING_METHOD_CONFIGURATIONS = {
         # Weight to give based on the age of the article.
         daily_decay_factor: {
@@ -43,20 +47,23 @@ module Articles
             [9, 0.55], [10, 0.5], [11, 0.4],
             [12, 0.3], [13, 0.2], [14, 0.1]
           ],
-          fallback: 0
+          fallback: 0,
+          requires_user: false
         },
         # Weight to give for the number of comments on the article
         # from other users that the given user follows.
         comment_count_by_those_followed_factor: {
-          clause: "COUNT(comments.id)",
+          clause: "COUNT(comments_by_followed.id)",
           cases: [[0, 0.95], [1, 0.98], [2, 0.99]],
-          fallback: 0.93
+          fallback: 0.93,
+          requires_user: true
         },
         # Weight to give to the number of comments on the article.
         comments_count_factor: {
           clause: "articles.comments_count",
           cases: [[0, 0.9], [1, 0.94], [2, 0.95], [3, 0.98], [4, 0.999]],
-          fallback: 1
+          fallback: 1,
+          requires_user: false
         },
         # Weight to give based on the difference between experience
         # level of the article and given user.
@@ -68,46 +75,53 @@ module Articles
               FROM users_settings WHERE users_settings.user_id = :user_id
             )))",
           cases: [[0, 1], [1, 0.98], [2, 0.97], [3, 0.96], [4, 0.95], [5, 0.94]],
-          fallback: 0.93
+          fallback: 0.93,
+          requires_user: true
         },
         # Weight to give when the given user follows the article's
         # author.
         following_author_factor: {
           clause: "COUNT(followed_user.follower_id)",
           cases: [[0, 0.8], [1, 1]],
-          fallback: 1
+          fallback: 1,
+          requires_user: true
         },
         # Weight to give to the when the given user follows the
         # article's organization.
         following_org_factor: {
           clause: "COUNT(followed_org.follower_id)",
           cases: [[0, 0.95], [1, 1]],
-          fallback: 1
+          fallback: 1,
+          requires_user: true
         },
         # Weight to give an article based on it's most recent comment.
         latest_comment_factor: {
           clause: "(current_date - MAX(comments.created_at)::date)",
           cases: [[0, 1], [1, 0.9988]],
-          fallback: 0.988
+          fallback: 0.988,
+          requires_user: false
         },
         # Weight to give for the number of intersecting tags the given
         # user follows and the article has.
         matching_tags_factor: {
           clause: "COUNT(followed_tags.follower_id)",
           cases: [[0, 0.4], [1, 0.9]],
-          fallback: 1
+          fallback: 1,
+          requires_user: true
         },
         # Weight to give for the number of reactions on the article.
         reactions_factor: {
           clause: "articles.reactions_count",
           cases: [[0, 0.9988], [1, 0.9988], [2, 0.9988], [3, 0.9988]],
-          fallback: 1
+          fallback: 1,
+          requires_user: false
         },
         # Weight to give based on spaminess of the article.
         spaminess_factor: {
           clause: "articles.spaminess_rating",
           cases: [[0, 1]],
-          fallback: 0.05
+          fallback: 0.05,
+          requires_user: false
         }
       }.freeze
 
@@ -151,24 +165,67 @@ module Articles
       # Articles::Feeds for both Basic and LargeForemExperimental as
       # well as the signed in and not signed in cases.
       def call
-        Article.find_by_sql([
-                              the_sql_statement,
-                              {
-                                user_id: @user.id,
-                                number_of_results: @number_of_articles.to_i,
-                                default_user_experience_level: @default_user_experience_level.to_i
-                              },
-                            ])
+        if @user.nil?
+          Article.find_by_sql([
+                                sql_for_nil_user,
+                                {
+                                  number_of_results: @number_of_articles.to_i
+                                },
+                              ])
+        else
+          Article.find_by_sql([
+                                sql_for_existing_user,
+                                {
+                                  user_id: @user.id,
+                                  number_of_results: @number_of_articles.to_i,
+                                  default_user_experience_level: @default_user_experience_level.to_i
+                                },
+                              ])
+        end
       end
 
       private
 
-      # The relevance score components speak.  Those method
-      # implementations are deeply entwined with the SQL statements.
+      # The sql statement for selecting based on relevance scores that
+      # are user required.
       #
       # @todo Remember to not use "SELECT articles.*" and also
       #   consider pagination and the interaction with Kaminari.
-      def the_sql_statement
+      def sql_for_nil_user
+        <<~THE_SQL_STATEMENT
+          WITH top_articles AS (
+            SELECT articles.id,
+            (#{relevance_score_components_as_sql}) AS relevance_score
+            FROM articles
+            LEFT OUTER JOIN comments
+              ON comments.commentable_id = articles.id
+                AND comments.commentable_type = 'Article'
+            WHERE published = true
+            GROUP BY articles.id,
+              articles.title,
+              articles.published_at,
+              articles.comments_count,
+              articles.experience_level_rating,
+              articles.spaminess_rating,
+              articles.reactions_count
+            ORDER BY relevance_score DESC,
+              articles.published_at DESC
+            #{offset_if_applicable} LIMIT :number_of_results)
+          SELECT articles.*
+          FROM articles
+          INNER JOIN top_articles
+            ON top_articles.id = articles.id
+          WHERE relevance_score > 0
+          ORDER BY articles.published_at DESC;
+        THE_SQL_STATEMENT
+      end
+
+      # The sql statement for selecting based on relevance scores that
+      # are user required.
+      #
+      # @todo Remember to not use "SELECT articles.*" and also
+      #   consider pagination and the interaction with Kaminari.
+      def sql_for_existing_user
         <<~THE_SQL_STATEMENT
           WITH top_articles AS (
             SELECT articles.id,
@@ -194,18 +251,20 @@ module Articles
                 AND followed_org.followable_type = 'Organization'
                 AND followed_org.follower_id = :user_id
                 AND followed_org.follower_type = 'User'
-            LEFT OUTER JOIN comments
-              ON comments.commentable_id = articles.id
-                AND comments.commentable_type = 'Article'
-                AND followed_user.followable_id = comments.user_id
+            LEFT OUTER JOIN comments AS comments_by_followed
+              ON comments_by_followed.commentable_id = articles.id
+                AND comments_by_followed.commentable_type = 'Article'
+                AND followed_user.followable_id = comments_by_followed.user_id
                 AND followed_user.followable_type = 'User'
             LEFT OUTER JOIN user_blocks
               ON user_blocks.blocked_id = articles.user_id
                 AND user_blocks.blocked_id IS NULL
                 AND user_blocks.blocker_id = :user_id
+            LEFT OUTER JOIN comments
+              ON comments.commentable_id = articles.id
+                AND comments.commentable_type = 'Article'
             WHERE published = true
             GROUP BY articles.id,
-              articles.title,
               articles.published_at,
               articles.comments_count,
               articles.experience_level_rating,
@@ -218,7 +277,8 @@ module Articles
           FROM articles
           INNER JOIN top_articles
             ON top_articles.id = articles.id
-            ORDER BY articles.published_at DESC;
+          WHERE relevance_score > 0
+          ORDER BY articles.published_at DESC;
         THE_SQL_STATEMENT
       end
 
@@ -252,6 +312,9 @@ module Articles
         # configurations, we're only accepting those as valid
         # configurations.
         SCORING_METHOD_CONFIGURATIONS.each_pair do |valid_method_name, default_config|
+          # Don't attempt to use this factor if we don't have user.
+          next if default_config.fetch(:requires_user) && @user.nil?
+
           # Ensure that we're only using a scoring configuration that
           # the caller provided.
           next unless scoring_configs.key?(valid_method_name)
--- spec/services/articles/feeds/weighted_query_strategy_spec.rb
+++ spec/services/articles/feeds/weighted_query_strategy_spec.rb
@@ -3,9 +3,18 @@ require "rails_helper"
 RSpec.describe Articles::Feeds::WeightedQueryStrategy, type: :service do
   subject(:feed_strategy) { described_class.new(user: user) }
 
-  let(:user) { create(:user) }
+  describe "with a nil user" do
+    let(:user) { nil }
 
-  describe "#call" do
+    it "#call performs a successful query" do
+      article = create(:article)
+      expect(feed_strategy.call).to match_array([article])
+    end
+  end
+
+  describe "with a non-nil user" do
+    let(:user) { create(:user) }
+
     describe "with modified :scoring_configs" do
       subject(:feed_strategy) { described_class.new(user: user, scoring_configs: scoring_configs) }
 
@@ -20,7 +29,7 @@ RSpec.describe Articles::Feeds::WeightedQueryStrategy, type: :service do
         }
       end
 
-      it "performs a successful query" do
+      it "#call performs a successful query" do
         # Yes, this is not a very exciting test.  However, the purpose
         # of the test is to see if the SQL statement runs.
         article = create(:article)
@@ -28,7 +37,7 @@ RSpec.describe Articles::Feeds::WeightedQueryStrategy, type: :service do
       end
     end
 
-    it "performs a successful query" do
+    it "#call performs a successful query" do
       # Yes, this is not a very exciting test.  However, the purpose
       # of the test is to see if the SQL statement runs.
       article = create(:article)
